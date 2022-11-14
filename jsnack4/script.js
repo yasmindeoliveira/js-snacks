ef7ccdf1359ff4aa959e2985338f46f2f7b9671d
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-14 17:34:03 +0100

    completato bonus

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,15 +1,16 @@
 //In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
-let array = ["Bryan", "Emanuele", "Monica", "Diego", "Veronica"];
+let array = ["bryan", "emanuele", "monica", "diego", "veronica"];
 let nome = prompt("Inserisca il suo nome completo:");
 let flag = false;
+nome = nome.toLowerCase();
+
 
 for (let i = 0; i < array.length; i++){
     if (nome == array[i]){
         flag = true;
     }
     
-
 };
 
 if (flag){
