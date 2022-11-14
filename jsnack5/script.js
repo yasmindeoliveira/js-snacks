25e8e1451638d576870497e01fa935c884206187
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-14 17:40:59 +0100

    completato bonus

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -1,13 +1,20 @@
 //Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array. Stampa l'array alla fine.
 
-let array = [];
-
+let arrayDispari = [];
+let arreyPari = [];
 for (let i = 0; i < 6; i++){
     let numero = prompt("Inserisca un numero:");
     numero = parseInt(numero);
 
     if (numero % 2 != 0){
-        array.push(numero);
-        console.log(array);
+        arrayDispari.push(numero);
+        
+
+    } else {
+        arreyPari.push(numero);
+        
     }
 }
+
+console.log("Numeri Dispari: " + arrayDispari);
+console.log("Numeri Pari: " + arreyPari);
